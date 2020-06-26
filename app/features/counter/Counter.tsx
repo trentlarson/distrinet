15d97e69ecec75cdd38de75a6740e9546564b998
Trent Larson <trent@trentlarson.com>
2020-06-26 15:26:43 -0600

    revert counter page to original functionality

--- app/features/counter/Counter.tsx
+++ app/features/counter/Counter.tsx
@@ -3,16 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../../constants/routes.json';
-import { dispatchCacheForAll, dispatchSettings } from '../distnet/distnetSlice';
-import { dispatchLoadAllTasks } from '../task-lists/taskListsSlice';
+import {
+  increment,
+  decrement,
+  incrementIfOdd,
+  incrementAsync,
+  selectCount,
+} from './counterSlice';
 
 export default function Counter() {
   const dispatch = useDispatch();
-  const distnet = useSelector((state: RootState) => state.distnet);
-  const settingsNum = distnet.settings.sources
-    ? distnet.settings.sources.length
-    : 0;
-  const settingsText = `${settingsNum} URI${settingsNum === 1 ? '' : 's'}`;
+  const value = useSelector(selectCount);
   return (
     <div>
       <div className={styles.backButton} data-tid="backButton">
@@ -21,34 +22,49 @@ export default function Counter() {
         </Link>
       </div>
       <div className={`counter ${styles.counter}`} data-tid="counter">
-        {settingsText}
+        {value}
       </div>
       <div className={styles.btnGroup}>
         <button
           className={styles.btn}
           onClick={() => {
-            dispatch(dispatchSettings());
+            dispatch(increment());
           }}
           data-tclass="btn"
           type="button"
         >
-          reload
+          <i className="fa fa-plus" />
         </button>
-        <ul>
-          {distnet.settings.sources &&
-            distnet.settings.sources.map((uriSource) => (
-              <li key={uriSource.id}>
-                {uriSource.name}
-                <ul>
-                  <li>
-                    {distnet.cache[uriSource.id]
-                      ? distnet.cache[uriSource.id].date
-                      : '(no local copy yet)'}
-                  </li>
-                </ul>
-              </li>
-            ))}
-        </ul>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(decrement());
+          }}
+          data-tclass="btn"
+          type="button"
+        >
+          <i className="fa fa-minus" />
+        </button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(incrementIfOdd());
+          }}
+          data-tclass="btn"
+          type="button"
+        >
+          odd
+        </button>
+        <button
+          className={styles.btn}
+          onClick={() => {
+            dispatch(incrementAsync());
+          }}
+          data-tclass="btn"
+          type="button"
+        >
+          async
+        </button>
       </div>
     </div>
   );
