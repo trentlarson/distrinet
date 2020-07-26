6cfde981131c4d0f3d6d5890c0dd91a7fa78cb6d
Trent Larson <trent@trentlarson.com>
2020-07-26 08:04:30 -0600

    fix indentation (no code/logic changes)

--- app/features/genealogy/js/tree.js
+++ app/features/genealogy/js/tree.js
@@ -98,84 +98,84 @@
     }
   }
 
-// Find person by ID in json object tree
-function find(obj, id) {
-  if (obj.id === id) return obj;
-  for (var i = 0; i < obj._parents.length; i ++) {
-    var result = find(obj._parents[i], id);
-    if (result) return result;
-  }
-  return null;
-}
-
-// Get children: Get their IDs from the display->familiesAsParent object. Get their names from the persons array.
-function getChildren(family, persons) {
-	var children = [];
-	// Iterate all children
-	for (var j=0; j < family.length; j++) {
-	  // Get name of child: Assume living if person not found in persons array
-		for (var i=1; i<persons.length; i++) {
-			if (family[j].resourceId == persons[i].id) {
-				var name = (persons[i].display.name);
-			  children.push({ id: persons[0].display.familiesAsParent[0].children[j].resource, name: name });
-			}
-		}
-	}
-	return children;
-}
-
-// Get the parents: Get their IDs from the display->familiesAsChild object. Get their names from the persons array.
-function getParents(family, persons) {
-	var parent1 = family.parent1.resourceId;
-	var parent2 = family.parent2.resourceId;
-	var parents = {father: null, mother: null};
-	// Iterate all persons
-	for (var i=1; i<persons.length; i++) {
-
-		// Look for first parent
-		if (persons[i].id == parent1) {
-			// Detect Father/Mother by gender
-			if (persons[i].gender.type == "http://gedcomx.org/Male") {
-				parents.father = {id: persons[i].id, name: persons[i].display.name, url: family.parent1.resource};
-			} else {
-				parents.mother = {id: persons[i].id, name: persons[i].display.name, url: family.parent1.resource};
-			}
-		}
-
-		// Look for second parent
-		if (persons[i].id == parent2) {
-			// Detect Father/Mother by gender
-			if (persons[i].gender.type == "http://gedcomx.org/Male") {
-				parents.father = {id: persons[i].id, name: persons[i].display.name, url: family.parent2.resource};
-			} else {
-				parents.mother = {id: persons[i].id, name: persons[i].display.name, url: family.parent2.resource};
-			}
-		}
-
-	}
-	return parents;
-}
-
-// Get Query parameters
-function getQueryParams() {
-  const vars = [];
-  let hash;
-  const hashes = window.location.href
-        .slice(window.location.href.indexOf('?') + 1)
-        .split('&');
-  for (let i = 0; i < hashes.length; i++) {
-    hash = hashes[i].split('=');
-    vars.push(hash[0]);
-    vars[hash[0]] = hash[1];
-  }
-  // Set a default location just for fun
-  if (vars.id == undefined) {
-    vars.id = 'https://raw.githubusercontent.com/misbach/familytree/master/people/KWCJ-RN4/KWCJ-RN4.json';
+  // Find person by ID in json object tree
+  function find(obj, id) {
+    if (obj.id === id) return obj;
+    for (var i = 0; i < obj._parents.length; i ++) {
+      var result = find(obj._parents[i], id);
+      if (result) return result;
+    }
+    return null;
   }
 
-  // if (vars.id == undefined) vars.id = "file:///Users/tlarson/backed-martin-rigby-default/Martin/KWCC-9SJ/KWCC-9SJ.json";
-  return vars;
-}
+  // Get children: Get their IDs from the display->familiesAsParent object. Get their names from the persons array.
+  function getChildren(family, persons) {
+    var children = [];
+    // Iterate all children
+    for (var j=0; j < family.length; j++) {
+      // Get name of child: Assume living if person not found in persons array
+      for (var i=1; i<persons.length; i++) {
+        if (family[j].resourceId == persons[i].id) {
+          var name = (persons[i].display.name);
+          children.push({ id: persons[0].display.familiesAsParent[0].children[j].resource, name: name });
+        }
+      }
+    }
+    return children;
+  }
+
+  // Get the parents: Get their IDs from the display->familiesAsChild object. Get their names from the persons array.
+  function getParents(family, persons) {
+    var parent1 = family.parent1.resourceId;
+    var parent2 = family.parent2.resourceId;
+    var parents = {father: null, mother: null};
+    // Iterate all persons
+    for (var i=1; i<persons.length; i++) {
+
+      // Look for first parent
+      if (persons[i].id == parent1) {
+        // Detect Father/Mother by gender
+        if (persons[i].gender.type == "http://gedcomx.org/Male") {
+          parents.father = {id: persons[i].id, name: persons[i].display.name, url: family.parent1.resource};
+        } else {
+          parents.mother = {id: persons[i].id, name: persons[i].display.name, url: family.parent1.resource};
+        }
+      }
+
+      // Look for second parent
+      if (persons[i].id == parent2) {
+        // Detect Father/Mother by gender
+        if (persons[i].gender.type == "http://gedcomx.org/Male") {
+          parents.father = {id: persons[i].id, name: persons[i].display.name, url: family.parent2.resource};
+        } else {
+          parents.mother = {id: persons[i].id, name: persons[i].display.name, url: family.parent2.resource};
+        }
+      }
+
+    }
+    return parents;
+  }
+
+  // Get Query parameters
+  function getQueryParams() {
+    const vars = [];
+    let hash;
+    const hashes = window.location.href
+          .slice(window.location.href.indexOf('?') + 1)
+          .split('&');
+    for (let i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    // Set a default location just for fun
+    if (vars.id == undefined) {
+      vars.id = 'https://raw.githubusercontent.com/misbach/familytree/master/people/KWCJ-RN4/KWCJ-RN4.json';
+    }
+
+    // if (vars.id == undefined) vars.id = "file:///Users/tlarson/backed-martin-rigby-default/Martin/KWCC-9SJ/KWCC-9SJ.json";
+    return vars;
+  }
 
 
   exports.setCache = setCache;
