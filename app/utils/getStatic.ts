80717f610bccb5f8ac470704991c872a0f145dc2
Trent Larson <trent@trentlarson.com>
2021-07-25 10:41:03 -0600

    change to use of getAppPath to refer to static resources

--- app/utils/getStatic.ts
+++ app/utils/getStatic.ts
@@ -1,24 +1,29 @@
-// from https://github.com/loopmode/electron-webpack-static-examples/blob/master/src/renderer/utils/getStatic.ts
+// originally from https://github.com/loopmode/electron-webpack-static-examples/blob/master/src/renderer/utils/getStatic.ts
 import electron from 'electron';
 import path from 'path';
+import fs from 'fs';
+import process from 'process';
 
-const isDevelopment =
-  process.env.NODE_ENV !== 'production' && !electron.remote.app.isPackaged;
+const dirname = module.__dirname;
 
-// see https://github.com/electron-userland/electron-webpack/issues/241#issuecomment-582920906
+const IS_PACKAGED = electron.remote.app.isPackaged;
+const APP_PATH = electron.remote.app.getAppPath();
+
 export default function getStatic(relativePath = '') {
-  if (isDevelopment) {
-    // A relative URL begins from just under the 'app' directory (for dev).
-
-    // Here's the solution from https://webpack.electron.build/using-static-assets
-    // return url.resolve(window.location.origin, relativePath); // requires at top: import * as url from 'url';
-    // window.location.origin ends up being 'file://' and 'url.resolve' ensures '/' before relative path if none exists
-
-    // However, let's not return a URL on dev and a file path on prod. Ug.
-
-    // Here's a path under the top level of the source (when running 'yarn dev').
-    return path.resolve('app/static', relativePath);
+  if (IS_PACKAGED) {
+    return path.resolve(APP_PATH, '..', 'static', relativePath);
   }
-  // __static is something magic from electron-webpack: https://webpack.electron.build/using-static-assets
-  return path.resolve(__static, relativePath);
+  // A relative URL begins from just under the 'app' directory (for dev).
+  return path.resolve(APP_PATH, 'static', relativePath);
+
+  // Here's a path under the top level of the source (when running 'yarn dev')
+  // ... but when packaged it always has a value of '/'
+  // I think __dirname is from https://nodejs.org/api/modules.html#modules_dirname
+  //return path.resolve(__dirname, 'app/static', relativePath);
+
+  // Here's the solution from https://webpack.electron.build/using-static-assets
+  // (However, let's not return a URL on dev and a file path on prod. Ug.)
+  // return url.resolve(window.location.origin, relativePath); // requires this at top: import * as url from 'url';
+  // window.location.origin ends up being 'file://' and 'url.resolve' ensures '/' before relative path if none exists
+
 }
